843a433a1f7726dc59da6926d6e3091988c12f12
finnbuhse <50274957+finnbuhse@users.noreply.github.com>
2022-11-27 23:08:20 +0000

    2 files modified

--- src/app/dataInput.component.ts
+++ src/app/dataInput.component.ts
@@ -21,5 +21,23 @@ export class DataInputComponent {
   addVariable()
   {
     this.variables.push(this.variable);
+    this.variable = "";
+  }
+
+  generateFunction()
+  {
+    for (var i = 0; i < this.datasetTexts.length; i++)
+    {
+      var tempString = this.datasetTexts[i].replace(" ", "");
+      console.log(tempString);
+      var tempArray = tempString.split(",");
+      console.log(tempArray);
+      for (var j = 0; j < tempArray.length; j++)
+      {
+        this.datasets[i][j] = parseFloat(tempArray[j]);
+      }
+    }
+
+    console.log(this.datasets);
   }
 }
