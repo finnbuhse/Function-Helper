4cfb3fe25cecf25d24ddda6edf31c548d33f3e61
finnbuhse <50274957+finnbuhse@users.noreply.github.com>
2022-12-10 18:51:31 +0000

    1 file modified

--- src/app/firestoreService.ts
+++ src/app/firestoreService.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "@firebase/app";
+import { getFirestore } from "@firebase/firestore";
 
 @Injectable() // Defines 'singleton' like behaviour.
 export class FirestoreService
