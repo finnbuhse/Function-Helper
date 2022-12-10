c108352cd0d3df6ff8a526001c07255c2df7358d
finnbuhse <50274957+finnbuhse@users.noreply.github.com>
2022-12-10 23:17:11 +0000

    1 file modified

--- src/app/firebaseService.ts
+++ src/app/firebaseService.ts
@@ -2,26 +2,26 @@ import { Injectable, OnInit } from "@angular/core";
 import { initializeApp } from "@firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBCajsw1DN4lzSpJJkfUrOUdKmEbZl66f4",
+  authDomain: "function-helper.firebaseapp.com",
+  //databaseURL: "https://function-helper.firebaseio.com",
+  projectId: "function-helper",
+  storageBucket: "function-helper.appspot.com",
+  messagingSenderId: "278533435669",
+  appId: "1:278533435669:web:1f54b8bc463f36b3e3791c",
+  measurementId: "G-F7MXM13S6S",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
 @Injectable() // Defines 'singleton' like behaviour.
 export class FirebaseService implements OnInit
 {
-  firebaseConfig = {
-    apiKey: "AIzaSyBCajsw1DN4lzSpJJkfUrOUdKmEbZl66f4",
-    authDomain: "function-helper.firebaseapp.com",
-    //databaseURL: "https://function-helper.firebaseio.com",
-    projectId: "function-helper",
-    storageBucket: "function-helper.appspot.com",
-    messagingSenderId: "278533435669",
-    appId: "1:278533435669:web:1f54b8bc463f36b3e3791c",
-    measurementId: "G-F7MXM13S6S",
-  };
-
-  app = initializeApp(this.firebaseConfig);
-
-  firestore: any;
-
   ngOnInit()
   {
-    this.firestore = getFirestore(this.app);
+
   }
 }
