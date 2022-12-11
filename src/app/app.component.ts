37bbcddecf6a017060aad1602d6c69dd04b79d14
finnbuhse <50274957+finnbuhse@users.noreply.github.com>
2022-12-11 10:24:56 +0000

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,9 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  constructor (private store: AngularFirestore)
+  {
+
+  }
 }
