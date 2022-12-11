288abf18e64123938a852d0e7bf224390a16ae06
finnbuhse <50274957+finnbuhse@users.noreply.github.com>
2022-12-11 10:16:55 +0000

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,23 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ChartsModule } from 'ng2-charts';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCajsw1DN4lzSpJJkfUrOUdKmEbZl66f4",
+  authDomain: "function-helper.firebaseapp.com",
+  databaseURL: "https://function-helper.eur3.firebaseio.com",
+  projectId: "function-helper",
+  storageBucket: "function-helper.appspot.com",
+  messagingSenderId: "278533435669",
+  appId: "1:278533435669:web:1f54b8bc463f36b3e3791c",
+  measurementId: "G-F7MXM13S6S",
+};
+
 import { AppComponent } from './app.component';
 import { GraphComponent } from './graph.component';
 import { FunctionInteractorComponent } from './functionInteractor.component';
@@ -18,7 +35,7 @@ import { SocketManager } from './socketManager';
 import { FirebaseService } from './firebaseService';
 
 @NgModule({
-  imports:      [ BrowserModule, HttpClientModule, BrowserAnimationsModule, FormsModule, MatInputModule, MatFormFieldModule, ChartsModule ],
+  imports:      [ BrowserModule, HttpClientModule, BrowserAnimationsModule, FormsModule, MatInputModule, MatFormFieldModule, ChartsModule, AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule ],
   declarations: [ AppComponent, GraphComponent, FunctionInteractorComponent, FunctionInputComponent, DataInputComponent, FileUploadComponent ],
   providers: [ SocketManager, FirebaseService ],
   bootstrap: [ AppComponent ]
--- src/app/firebaseService.ts
+++ src/app/firebaseService.ts
@@ -1,29 +1,6 @@
 import { Injectable, OnInit } from "@angular/core";
-import { AngularFireModule } from "@angular/fire/compat";
-//import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-//import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-//import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBCajsw1DN4lzSpJJkfUrOUdKmEbZl66f4",
-  authDomain: "function-helper.firebaseapp.com",
-  databaseURL: "https://function-helper.eur3.firebaseio.com",
-  projectId: "function-helper",
-  storageBucket: "function-helper.appspot.com",
-  messagingSenderId: "278533435669",
-  appId: "1:278533435669:web:1f54b8bc463f36b3e3791c",
-  measurementId: "G-F7MXM13S6S",
-};
 
-const environment = {
-  production: false,
-  firebase: firebaseConfig
-}
-
-@Injectable({ // Defines 'singleton' like behaviour.
-  imports: [ AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule ]
-})
 export class FirebaseService implements OnInit
 {
 
